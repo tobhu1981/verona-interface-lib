cfe01fc06e838e4536b8d09fd36424fac2cf6130
tobhu1981 <tobias.huste@hu-berlin.de>
2026-03-03 10:04:48 +0100

    Single source of truth  for package- and spec-version

--- packages/player/src/constants.ts
+++ packages/player/src/constants.ts
@@ -2,11 +2,22 @@
 // CONSTANTS
 // ============================================================================
 
+import pkg from '../package.json';
+
 /** @internal Default target origin for postMessage */
 export const DEFAULT_TARGET_ORIGIN = '*';
 
 /** @internal Minimum length for SharedParameter keys */
 export const MIN_SHARED_PARAMETER_KEY_LENGTH = 2;
 
-/**  @public hard coded verona api spec version*/
-export const VERONA_SPEC_VERSION = '6.1.1';
+/**
+ * NPM package version of @verona/player library
+ * @public
+ */
+export const PACKAGE_VERSION = pkg.version;
+
+/**
+ * Verona Player Specification version implemented by this library
+ * @public
+ */
+export const VERONA_SPEC_VERSION = pkg.veronaSpec;
