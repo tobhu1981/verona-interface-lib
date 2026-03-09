891e7f544f4f325d252461064de775ac8a7a6906
tobhu1981 <tobias.huste@hu-berlin.de>
2026-03-09 10:10:23 +0100

    Update widget types values

--- packages/widget/src/types/values.ts
+++ packages/widget/src/types/values.ts
@@ -9,15 +9,4 @@
  */
 export namespace AllowedPropertiesValues {
 
-  /** Specifies the access level of the editing person ('super' = maximal) */
-  export type Role = 'guest' | 'commentator' | 'developer' | 'maintainer' | 'super';
-
-  /** Specifies type for dependency */
-  export type DependencyType = 'file' | 'service';
-
-  /** Specifies type for variable info */
-  export type VariableInfoType = 'string' | 'integer' | 'number' | 'boolean' | 'attachment' | 'json' | 'no-value' | 'coded';
-
-   /** Data type format */
-  export type Format = 'text-selection' | 'image' | 'capture-image' | 'audio' | 'ggb-file' | 'non-negative' | 'latex' | 'math-ml' | 'math-table' | 'math-text-mix' | 'ggb-variable' | '';
-}
+};
