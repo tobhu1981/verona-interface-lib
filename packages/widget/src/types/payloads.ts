9194ff0099858f18a90a307763dfbf5dc6d7403b
tobhu1981 <tobias.huste@hu-berlin.de>
2026-03-09 10:08:44 +0100

    Update widget types payload

--- packages/widget/src/types/payloads.ts
+++ packages/widget/src/types/payloads.ts
@@ -6,43 +6,61 @@ import { MainSchema } from './schemas';
 
 /**
  * Namespace containing all message payload interfaces.
- * These define the structure of data sent in Verona messages.
+ * These define the structure of data sent in Verona Widget messages.
  * @public
  */
 export namespace PayloadInterfacesProperties {
 
-  /** Namespace containing all editor receives payloads @public*/
-  export namespace EditorReceive {
+  /** Namespace containing all widget receive payloads @public */
+  export namespace WidgetReceive {
 
-    /** voeStartCommand - Host-> Editor @public*/
+    /**
+     * vowStartCommand - Host->Widget: Host sends execution parameters.
+     * @public
+     */
     export interface StartCommand {
       sessionId: MainSchema.SessionIdString;
-      unitDefinition?: string; // format:byte
-      unitDefinitionType?: string;
-      editorConfig?: MainSchema.EditorConfig[];
+      widgetConfig?: MainSchema.WidgetConfig[];
+      sharedParameters?: MainSchema.SharedParameter[];
+      state?: string; // format: byte
     }
-  
+
   }
-  
-  /** Namespace containing all editor send payloads @public*/
-  export namespace EditorSend {
- 
-     /** voeReadyNotification - Editor->Host @public*/
-    export interface ReadyNotificationData {
+
+  /** Namespace containing all widget send payloads @public */
+  export namespace WidgetSend {
+
+    /**
+     * vowReadyNotification - Widget->Host: Widget announces readiness.
+     * @public
+     */
+    export interface ReadyNotification {
       metadata: string; // Stringified JSON-LD metadata
     }
-    
-    /** voeDefinitionChangedNotification - Editor->Host @public*/
-    export interface DefinitionChangedNotification {
+
+    /**
+     * vowStateChangedNotification - Widget->Host: Widget sends state data.
+     * @public
+     */
+    export interface StateChangedNotification {
       sessionId: MainSchema.SessionIdString;
       timeStamp: string; // format: ISO 8601 date-time
-      unitDefinition?: string; // format: byte
-      unitDefinitionType?: string;
-      variables?: MainSchema.VariableInfo[];
-      dependenciesToPlay?: MainSchema.Dependency[];
-      dependenciesToEdit?: MainSchema.Dependency[];
       sharedParameters?: MainSchema.SharedParameter[];
-      }
+      state?: string; // format: byte
+    }
+
+    /**
+     * vowReturnRequested - Widget->Host: Widget calls for closing the widget dialog.
+     * @public
+     */
+    export interface ReturnRequested {
+      sessionId: MainSchema.SessionIdString;
+      timeStamp: string; // format: ISO 8601 date-time
+      sharedParameters?: MainSchema.SharedParameter[];
+      state?: string; // format: byte
+      /** If true, the host is requested to send the final state to the player. Default: true */
+      saveState?: boolean; // default: true
+    }
 
   }
 
